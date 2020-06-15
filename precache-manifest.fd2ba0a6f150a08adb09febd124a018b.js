eec419b3a485d8b5b7b1f443b71c30d5d91d5a13
mlbghoon <mlbg_hoon@naver.com>
2020-06-15 16:29:37 +0900

    Updates

---
+++ precache-manifest.fd2ba0a6f150a08adb09febd124a018b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be10d5fd4e952cfc65aed4c92f18a787",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "691e0e72116bc2dab0d3",
+    "url": "/test/static/css/main.e9d703a3.chunk.css"
+  },
+  {
+    "revision": "119a140b96a2d079dcee",
+    "url": "/test/static/js/2.e1a151d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.e1a151d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691e0e72116bc2dab0d3",
+    "url": "/test/static/js/main.c6381d25.chunk.js"
+  },
+  {
+    "revision": "8d1db41edd2c7718b110",
+    "url": "/test/static/js/runtime-main.ce53c38e.js"
+  }
+]);
